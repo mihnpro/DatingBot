e45b0f9afa9a864c7775db04f62815e7a182b4f8
mihnpro <mihnpro@gmail.com>
2026-03-28 11:04:13 +0300

    Added conteinerisation

---
+++ services/UserService/main.py
@@ -0,0 +1,10 @@
+# """
+# UserService entrypoint.
+
+# Run locally:
+#   uvicorn user_service.api.main:app --reload --port 8001
+# """
+
+# from user_service.api.main import app
+
+# __all__ = ["app"]
